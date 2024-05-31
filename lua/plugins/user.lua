a5c97d4b3fede7220d08b34dfeaf2c08c9d42580
Branden Palmer <brandenpalmer08@gmail.com>
2024-05-31 22:38:52 +0700

    migrate user

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
@@ -46,7 +46,7 @@ return {
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require "astronvim.plugins.configs.luasnip" (plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom luasnip configuration such as filetype extend or custom snippets
       local luasnip = require "luasnip"
       luasnip.filetype_extend("javascript", { "javascriptreact" })
@@ -56,7 +56,7 @@ return {
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require "astronvim.plugins.configs.nvim-autopairs" (plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom autopairs configuration such as custom rules
       local npairs = require "nvim-autopairs"
       local Rule = require "nvim-autopairs.rule"
@@ -64,22 +64,91 @@ return {
       npairs.add_rules(
         {
           Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
+          -- don't add a pair if the next character is %
+              :with_pair(cond.not_after_regex "%%")
+          -- don't add a pair if  the previous character is xxx
+              :with_pair(
+                cond.not_before_regex("xxx", 3)
+              )
+          -- don't move right when repeat character
+              :with_move(cond.none())
+          -- don't delete if the next character is xx
+              :with_del(cond.not_after_regex "xx")
+          -- disable adding a newline when you press <cr>
+              :with_cr(cond.none()),
         },
         -- disable for .vim files, but it work for another filetypes
         Rule("a", "a", "-vim")
       )
     end,
+  },
+  {
+    "monsonjeremy/onedark.nvim",
+    -- branch = "treesitter",
+    opts = {
+      colors = {
+        bg = "#0a0c10",
+      },
+    },
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      background_colour = "NotifyBackground",
+      fps = 30,
+      level = 2,
+      minimum_width = 50,
+      max_width = 200,
+      render = "compact",
+      stages = "static",
+      timeout = 2000,
+      top_down = true,
+    },
+  },
+  {
+    "delphinus/vim-firestore",
+    event = "BufEnter *.rules",
+  },
+  {
+    "evesdropper/luasnip-latex-snippets.nvim",
+  },
+  -- edits the directory in buffer press '-' to activate
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    keys = {
+      { "-", "<cmd>Oil<cr>", mode = { "n" }, desc = "Open parent directory" },
+    },
+  },
+  {
+    "Wansmer/treesj",
+    keys = { "<leader>m", "<leader>j", "<leader>S" },
+    opts = {
+      max_join_length = 240,
+      max_split_length = 240,
+    },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("treesj").setup { --[[ your config ]]
+      }
+    end,
+  },
+  {
+    "danymat/neogen",
+    opts = {},
+    keys = {
+      { "<leader>lj", "<cmd>Neogen<cr>", mode = { "n" }, desc = "Create JsDoc" },
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
   },
 }
