663184bd7a98a2149d73abed9ae07d8f753c61ba
Branden Palmer <brandenpalmer08@gmail.com>
2024-06-01 16:33:44 +0700

    remove lsp sig highlight because its annoying

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -9,11 +9,11 @@ return {
   -- == Examples of Adding Plugins ==
 
   "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function() require("lsp_signature").setup() end,
+  -- },
 
   -- == Examples of Overriding Plugins ==
 
