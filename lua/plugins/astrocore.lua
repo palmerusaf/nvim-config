b48cba849cb50d8a8378d9a4c09e3a35a4d513e8
Branden Palmer <brandenpalmer08@gmail.com>
2024-11-20 16:59:11 +0700

    turn spell check on by default

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -29,7 +29,7 @@ return {
       opt = { -- vim.opt.<key>
         relativenumber = true, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
+        spell = true, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = true, -- sets vim.opt.wrap
       },
