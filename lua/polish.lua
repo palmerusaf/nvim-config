7545c9d2cc14d36a49f4b4415754312302850ff8
Branden Palmer <brandenpalmer08@gmail.com>
2025-03-13 22:23:17 +0700

    add hyper config lsp

--- lua/polish.lua
+++ lua/polish.lua
@@ -42,3 +42,16 @@ vim.api.nvim_create_autocmd("InsertEnter", {
     end
   end,
 })
+
+-- Hyprlang LSP
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "*.conf" },
+  callback = function(event)
+    print(string.format("starting hyprls for %s", vim.inspect(event)))
+    vim.lsp.start {
+      name = "hyprlang",
+      cmd = { "hyprls" },
+      root_dir = vim.fn.getcwd(),
+    }
+  end,
+})
