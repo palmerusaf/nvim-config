6a4d9b5f2506e9e4d235cf7b293f73e4501a261d
Branden Palmer <brandenpalmer08@gmail.com>
2026-04-29 13:33:48 -0500

    fix avante lsp issue

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,16 +1,37 @@
-return { -- override nvim-cmp plugin
-  "hrsh7th/nvim-cmp",
-  -- override the options table that is used in the `require("cmp").setup()` call
-  opts = function(_, opts)
-    -- opts parameter is the default options table
-    -- the function is lazy loaded so cmp is able to be required
-    local cmp = require "cmp"
-    -- modify the sources part of the options table
-    opts.sources = cmp.config.sources {
-      { name = "luasnip", priority = 1000 },
-      { name = "nvim_lsp", priority = 750 },
-      { name = "buffer", priority = 500 },
-      { name = "path", priority = 250 },
-    }
-  end,
+return {
+  "saghen/blink.cmp",
+  optional = true,
+  dependencies = {
+    -- add the legacy cmp source as a dependency for `blink.cmp`
+    "kdheepak/cmp-latex-symbols",
+  },
+  specs = {
+    -- install the blink, nvim-cmp compatibility layer
+    { "saghen/blink.compat", version = "*", lazy = true, opts = {} },
+  },
+  opts = {
+    sources = {
+      default = { "avante_commands", "avante_mentions", "avante_files" },
+      providers = {
+        avante_commands = {
+          name = "avante_commands",
+          module = "blink.compat.source",
+          score_offset = 90, -- show at a higher priority than lsp
+          opts = {},
+        },
+        avante_files = {
+          name = "avante_commands",
+          module = "blink.compat.source",
+          score_offset = 100, -- show at a higher priority than lsp
+          opts = {},
+        },
+        avante_mentions = {
+          name = "avante_mentions",
+          module = "blink.compat.source",
+          score_offset = 1000, -- show at a higher priority than lsp
+          opts = {},
+        },
+      },
+    },
+  },
 }
